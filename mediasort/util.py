74baea20913041eea204edc59f6bf992144289c9
Louis Lang <louis@louislang.com>
2019-12-07 23:14:01 -0600

    Added filemagic as fallback for identifying filetype

--- mediasort/util.py
+++ mediasort/util.py
@@ -1,6 +1,6 @@
+import magic
 import filetype
 from enum import Enum
-
 
 class Safety(Enum):
     SAFE = 1
@@ -11,9 +11,16 @@ class Safety(Enum):
 def get_mimetype(path):
     """ Get the mimetype of the given file pointer.
         @param  path    Path to a file to get the MIME for.
-        @return     A string representing the mimetype. """
-    return filetype.guess(path).mime
+        @return     A string representing the mimetype or `None` if no MIME
+                    coul be discerned. """
+    guess = filetype.guess(path)
 
+    if guess:
+        return guess.mime
+
+    # If we were unable to guess the MIME, attempt to use filemagic.
+    with magic.Magic() as m:
+        return m.id_filename(path)
 
 def is_video(mime):
     """ Returns `True` if the specified file pointer represents a video.
