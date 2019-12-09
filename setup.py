4d57fb22d289284dcd20e1feb83eada7b448ea7c
Louis Lang <louis@louislang.com>
2019-12-08 22:46:09 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
+from os import path
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='sortmedia',
-      version='1.1.0',
+      version='1.2.0',
       description='Dedupe and sort photo and video files based on the date ' +
                   'they were created.',
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       author='Louis Lang',
       author_email='louis@lang.sh',
       url='https://github.com/LouisLang/sortmedia',
@@ -16,4 +26,5 @@ setup(name='sortmedia',
             'sortmedia=sortmedia.cli:main',
         ],
       },
+      install_requires=requirements,
      )
